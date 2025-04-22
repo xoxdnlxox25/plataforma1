f7c50b9038ad464f7af338cc7293ae767d579331
xoxdnlxox25 <dnl25fl@gmail.com>
2025-04-22 10:53:11 -0400

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,2 +1,2 @@
 // Pega aquí tu URL del Apps Script (la crearemos luego)
-const URL = "https://script.google.com/macros/s/AKfycbx8kSr0_vffVF6QVbA8f7D6S6q85Of2xoEKuuMKv6M9QcAwkklBnMwqrJ5UXuuLHbDb/exec";
+const URL = "https://script.google.com/macros/s/AKfycbzbCIEfp3CAB1mnJ9mWMfHRw6SvANXHmm51wkiQkDIFpKo_suHhrkwKY4pbqggq8VqO/exec";
